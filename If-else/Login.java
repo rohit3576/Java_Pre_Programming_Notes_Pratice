e5e417fe681b4a84ef5492bc9dc790bc5e9a0585
rohit3576 <rohit03576@gmail.com>
2026-01-19 22:00:46 +0530

    Added complete If-else

---
+++ If-else/Login.java
@@ -0,0 +1,13 @@
+class Login{
+    public static void main(String[] args) {
+        String username="admi";
+        String password="pass234";
+        System.out.println("Loginb");
+        if(username=="admin" && password=="pass234"){
+            System.out.println("Logined.....");
+        }
+        else{
+            System.out.println("galat daal diya bhaiii............");
+        }
+    }
+}
---
+++ If-else/Range.java
@@ -0,0 +1,18 @@
+public class Range {
+    public static void main(String[] args) {
+        int x=10;
+        
+        System.out.println("sdjhbhcvhasdbvlaisdblh");
+        if(x>100){
+            System.out.println("large numbers");
+        }
+        else if(x>=11 && x<=100){
+            System.out.println("It is medium number");
+        }
+        else{
+            System.out.println("It is small number");
+        }
+
+    }
+    
+}
