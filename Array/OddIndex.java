d49ca7dfe0c419e69e68a4196e6e66ae43748fd5
rohit3576 <rohit03576@gmail.com>
2026-02-05 08:43:05 +0530

    even and odd index

--- Array/OddIndex.java
+++ Array/OddIndex.java
@@ -1,8 +1,8 @@
-class EvenIndex{
+class OddIndex{
     public static void main(String[] args) {
         int[] arr={45,345,2345,43523,3562,35625,5667,78,67868,688478,3245,25151,52667,4642};
         for(int i=0; i<arr.length;i++){
-            if(i%2==0){
+            if(i%2==1){
                 System.out.println(arr[i]);
             }
         }
