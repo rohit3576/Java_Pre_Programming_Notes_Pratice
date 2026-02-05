1e2d480b7b0c889d52bc8ea6e35be0f1c5085b56
rohit3576 <rohit03576@gmail.com>
2026-02-05 08:54:56 +0530

    reverse array

---
+++ Array/Reverse.java
@@ -0,0 +1,10 @@
+class Reverse{
+    public static void main(String[] args) {
+        int[] arr={45,345,2345,43523,3562,35625,5667,78,67868,688478,3245,25151,52667,4642};
+        
+        for(int i=arr.length-1; i>=0;i--){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
