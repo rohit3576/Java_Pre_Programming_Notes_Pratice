a6cb667d1b9995da833608b69140d93fdb14aad4
rohit3576 <rohit03576@gmail.com>
2026-02-05 08:39:10 +0530

    array started

---
+++ Array/EvenIndex.java
@@ -0,0 +1,9 @@
+class EvenIndex{
+    public static void main(String[] args) {
+        int[] arr= new int[500];
+        for(int i=0; i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
