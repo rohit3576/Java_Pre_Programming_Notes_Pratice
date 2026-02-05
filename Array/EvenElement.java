a306c793ad4e2c548a8e9e158c2e42f198ea5ebc
rohit3576 <rohit03576@gmail.com>
2026-02-05 08:46:26 +0530

    even odd element

---
+++ Array/EvenElement.java
@@ -0,0 +1,11 @@
+class EvenElement{
+    public static void main(String[] args) {
+        int[] arr={45,345,2345,43523,3562,35625,5667,78,67868,688478,3245,25151,52667,4642};
+        for(int i=0; i<arr.length;i++){
+            if(arr[i]%2==0){
+                System.out.println(arr[i]);
+            }
+        }
+
+    }
+}
