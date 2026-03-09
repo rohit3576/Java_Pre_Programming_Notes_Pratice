c00f08dbea0ed6716054f5ce739966bb69854d57
rohit3576 <rohit03576@gmail.com>
2026-03-09 12:38:22 +0530

    pratice

--- Encapsulation/Demo.java
+++ Encapsulation/Demo.java
@@ -1,15 +1,25 @@
+
+class Engine{
+    int id;
+}
+class Car extends Engine{
+    Engine e= new Engine();
+}
 class Demo{
     private int id;
     private String name;
 
-    public static String getData(String name){
+    public String getData(String name){
         return name;
     }
-    public static void setData(int id, String name){
+    public void setData(int id, String name){
         this.id=id;
         this.name=name;
     }
     public static void main(String[] args) {
+        Car c= new Car();
+        System.out.println(c.id); 
+
         
     }
 }
