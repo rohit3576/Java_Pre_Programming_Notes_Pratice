1382e847a5fc3a44a20068263f9c073328779ba0
rohit3576 <rohit03576@gmail.com>
2026-03-12 12:23:33 +0530

    Meta Mini Project

---
+++ Inheritance/Meta/MetaData.java
@@ -0,0 +1,12 @@
+package Meta;
+
+public class MetaData {
+    int id;
+    String name;
+    String password;
+
+    public void thankyou(){
+        System.out.println("Thank you "+name+" Your id is : "+id);
+    }
+    
+}
