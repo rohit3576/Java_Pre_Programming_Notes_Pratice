ecedf8e88d1799f9f176226749d94bcb7f0f059c
rohit3576 <rohit03576@gmail.com>
2026-03-16 23:56:42 +0530

    pratice

--- Pratice/Prac.java
+++ Pratice/Prac.java
@@ -1,9 +1,20 @@
 class School{
+    String name;
+    int id;
 
+}
+class Student extends School{
+    public void print(){
+        System.out.println(name+"this was name and "+id+" this is id " );
+    }
 }
 
 public class Prac{
     public static void main(String[] args) {
         System.out.println("This is pratice");
+        Student s= new Student();
+        s.id=1224;
+        s.name="Rohit";
+        s.print();
     }
 }
