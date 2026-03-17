31bc68881012a58d8b207fe4461a4fc884b51a60
rohit3576 <rohit03576@gmail.com>
2026-03-17 23:58:09 +0530

    string pratice

---
+++ Pratice/StringPrac.java
@@ -0,0 +1,19 @@
+class College {
+    int id;
+    String name;
+}
+
+class VCET extends College{
+    public void clgDetail(){
+        System.out.println(id+" This is id ");
+    }
+}
+
+
+
+public class StringPrac {
+
+    public static void main(String[] args) {
+        System.out.println("kjsfdlkb");
+    }
+}
