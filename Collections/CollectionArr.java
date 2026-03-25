8af01df1670aedf0241986b174a3634d89979ba4
rohit3576 <rohit03576@gmail.com>
2026-03-25 22:28:27 +0530

    Collections list

---
+++ Collections/CollectionArr.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class CollectionArr{
+    public static void main(String[] args){
+    ArrayList<Object> list = new ArrayList();
+    list.add("Hello");
+    list.add(23);
+    list.add(true);
+    list.add("Hellosf");
+
+    System.out.println(list);
+
+
+    }
+}
