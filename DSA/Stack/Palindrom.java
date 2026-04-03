65e71dbbd62a7550e6a419d2990790041b174f4a
rohit3576 <rohit03576@gmail.com>
2026-04-03 12:58:31 +0530

    pratice

--- DSA/Stack/Palindrom.java
+++ DSA/Stack/Palindrom.java
@@ -1,9 +1,9 @@
-
 import java.util.Stack;
 
-public class Reverse {
+public class Palindrom {
     public static void main(String[] args) {
-        String st= "ROhit";
+        
+        String st= "NAYAN";
 
         Stack ls= new Stack<>();
 
@@ -11,6 +11,7 @@ public class Reverse {
             ls.push(st.charAt(i));
         }
         System.out.println(ls);
+
         
         String rev="";
         System.out.println(rev);
@@ -18,6 +19,14 @@ public class Reverse {
             rev+=ls.pop();
         }
         System.out.println("Rversed: "+rev);
+        System.out.println(st);
+
+        if(rev.equals(st)){
+            System.out.println("It is a plaindrome");
+        }
+        else{
+            System.out.println("Not a palindrome");
+        }
 
 
     }
