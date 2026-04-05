ebfc3435e518394cf9721c18e02375f9da0aebdf
rohit3576 <rohit03576@gmail.com>
2026-04-05 16:19:55 +0530

    Basic Queu

---
+++ DSA/Queu/Basic.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Basic {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> q= new PriorityQueue<>();
+        q.offer(45);
+        q.offer(4436);
+        q.offer(5465);
+        q.offer(345345);
+        q.offer(565);
+        q.offer(4365);
+        System.out.println(q);
+        
+
+    }
+}
